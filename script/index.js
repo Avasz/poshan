92a064a3a0c62aef33621712ba213768cee06de6
Avasz <avashmulmi@gmail.com>
2015-05-29 15:08:51 +0545

    first

---
+++ script/index.js
@@ -0,0 +1,27 @@
+$("#splash").click (function(){
+	window.location.href = 'pages/home_screen.html'
+});
+
+$("#calc").click (function() {
+
+	$("#home_screen").fadeOut();
+	
+      location.href = 'diet.html';
+
+  
+});
+
+$("#detail_btn").click (function () {
+	
+window.location.href =  'more_details.html';
+});
+
+$("#go_back").click (function () {
+	
+window.location.href =  'diet.html';
+});
+
+$("#to_mp").click (function () {
+window.location.href = 'home_screen.html';
+});
+
